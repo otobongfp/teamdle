902793e0320f60cb229ac92aa41bc5047122606e
Otobong <otobongfp@gmail.com>
2022-10-03 17:30:08 +0100

    first commit for the haskell project

---
+++ app/Main.hs
@@ -0,0 +1,15 @@
+module Main where
+
+main :: IO ()
+main = putStrLn "Hello, Haskell!"
+
+
+-- Input the number of students.
+-- Input the number of teams.
+-- Divide the number of student by number of teams to know how many persons per team
+-- Start inputting teammates [name, timezone, ranking/10]
+-- Iterate till = Number of students.
+-- Output the different teams. (Team A, Team B.... n=)
+
+
+
